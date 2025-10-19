824c8d92e8fc4bc73a1e9d69892b95d4a715d3e1
zakzakst <e1433104@gmail.com>
2025-10-19 14:18:28 +0900

    fix

---
+++ old/init-user.sql
@@ -0,0 +1,3 @@
+create table user ( id int, name varchar(32), email varchar(32));
+insert user (id, name, email) values (1, 'John Doe', 'john@example.com');
+insert user (id, name, email) values (2, 'Jane Doe', 'jane@example.com');
